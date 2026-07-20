cd672b413d474b59d6cd66aca7c5a56c81879f94
Youg-Otricked <yougotricked2@gmail.com>
2026-07-19 18:43:08 -0700

    update

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@
 #include "json.hpp"
 #include <sys/stat.h>
 #include <ranges>
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+#endif
 const std::string QCVM_VERSION = "2.1.1";
 #include <unordered_set>
 std::unordered_set<std::string> load_tagged_versions() {
